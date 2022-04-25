2ba6e63cc3a1cef1f538eeb886d0c84919ce8e39
Sonam Samdupkhangsar <sonamsamdupkhangsar@Sonams-MacBook-Pro.local>
2022-04-25 13:45:00 -0600

    use full url

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ const router = new VueRouter({
     { path: '/implicit/callback', component: Auth.handleCallback() },        
   ]
 })
-//const vueUrl = 'https://sonamsamdupkhangsar.github.io/vue-catalog-ui'
+const vueUrl = 'https://sonamsamdupkhangsar.github.io/vue-catalog-ui'
 //const vueUrl = "http://localhost:8080"
 
 Vue.use(VueRouter)
@@ -21,7 +21,7 @@ console.log('issuer: ', process.env)
 Vue.use(Auth, {
   issuer: 'https://dev-975443.okta.com/oauth2/default',
   clientId: '0oasdq16pWdVhJl9W4x6',  
-  redirectUri: window.location.origin  + '/implicit/callback', // Handle the response from Okta and store the returned tokens.
+  redirectUri: vueUrl + '/implicit/callback', // Handle the response from Okta and store the returned tokens.
   scopes: ['openid', 'profile', 'email'],
   pkce: true 
 })
